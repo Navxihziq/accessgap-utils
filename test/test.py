d0cb800ad50656df0f615397022ba12bc6a8fd51
Navxihziq <zhixuanqi@outlook.com>
2024-09-24 12:45:22 -0400

    feat: overpy alternative for regex

--- test/test.py
+++ test/test.py
@@ -1,14 +1,16 @@
 import sys
+from datetime import datetime
 from pathlib import Path
 
 import geopandas as gpd
 import osmnx as ox
 import pytest
+from shapely.geometry import Polygon
 
 # Add the parent directory to sys.path
 sys.path.append(str(Path(__file__).parent.parent))
 
-from accessgap_utils import pois_from_polygon
+from accessgap_utils import OverpassFilter, OverpassQuery, pois_from_polygon
 
 
 def assert_non_empty_gdf(gdf: gpd.GeoDataFrame) -> None:
@@ -39,3 +41,41 @@ def test_datetime() -> None:
     ).geometry.iloc[0]
     gdf = pois_from_polygon(polygon, date=date, cache=False)
     assert_non_empty_gdf(gdf)
+
+
+@pytest.mark.overpass_coordstr
+def test_overpass_coordstr() -> None:
+    """Test if the OverpassFilter.__str__ method is working correctly"""
+    polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+    filter = OverpassFilter(polygon)
+    assert (
+        filter.polygon_coords_str()
+        == "0.000000 0.000000 1.000000 0.000000 1.000000 1.000000 0.000000 1.000000 0.000000 0.000000"
+    )
+
+
+@pytest.mark.overpass_query
+def test_overpass_query() -> None:
+    """Test if the OverpassQuery.__str__ method is working correctly"""
+    polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+    query = OverpassQuery(polygon)
+    assert (
+        str(query)
+        == "[out:json]; (node(poly:'0.000000 0.000000 1.000000 0.000000 1.000000 1.000000 0.000000 1.000000 0.000000 0.000000'); "
+        "way(poly:'0.000000 0.000000 1.000000 0.000000 1.000000 1.000000 0.000000 1.000000 0.000000 0.000000'); "
+        "relation(poly:'0.000000 0.000000 1.000000 0.000000 1.000000 1.000000 0.000000 1.000000 0.000000 0.000000');) out body;"
+    )
+
+
+@pytest.mark.overpass_query
+def test_overpass_preamble() -> None:
+    """Test if the OverpassQuery.__str__ method is working correctly"""
+    polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+    query = OverpassQuery(polygon, timeout=10, maxsize=10000, date=datetime(2017, 3, 9))
+    assert (
+        str(query)
+        == "[out:json][timeout:10][maxsize:10000][date:'2017-03-09T00:00:00Z']; (node(poly:'0.000000 0.000000 1.000000 0.000000 1.000000 1.000000 0.000000 1.000000 0.000000 0.000000'); "
+        "way(poly:'0.000000 0.000000 1.000000 0.000000 1.000000 1.000000 0.000000 1.000000 0.000000 0.000000'); "
+        "relation(poly:'0.000000 0.000000 1.000000 0.000000 1.000000 1.000000 0.000000 1.000000 0.000000 0.000000');) out body;"
+    )
+
