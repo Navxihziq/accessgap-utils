4e50783840c39b1cfa7a8491a1d34ff109c3419e
Navxihziq <zhixuanqi@outlook.com>
2024-09-24 02:04:15 -0400

    chore: included req in setup

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+# Read long description from README.md
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "Utility functions for Urban Access Gap Project"
+
 setup(
     name="accessgap-utils",
     version="0.1.0",
@@ -12,7 +19,7 @@ setup(
     author="Zhixuan Qi",
     author_email="zhixuanqi@outlook.com",
     description="Utility functions for Urban Access Gap Project",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/accessgap-utils",
     classifiers=[
