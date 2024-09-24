542f504ee2d1bbe2777ee6bcba20ee67989b735a
Navxihziq <zhixuanqi@outlook.com>
2024-09-24 02:20:01 -0400

    fix: removed irrelavent info from setup

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="accessgap-utils",
-    version="0.1.0",
-    packages=find_packages(),
-    author="Zhixuan Qi",
-    author_email="zhixuanqi@outlook.com",
-    description="Utility functions for Urban Access Gap Project",
-    url="https://github.com/Navxihziq/accessgap-utils",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+setup(py_modules=["accessgap_utils"])
