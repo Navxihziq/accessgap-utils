0d5b8e0b4f88c252592cb3cc4adf52c2052bc895
Navxihziq <zhixuanqi@outlook.com>
2024-09-24 02:28:02 -0400

    fix: removed irrelavent info from setup

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup(py_modules=["accessgap_utils"])
+setup(
+    name="accessgap-utils",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "geopandas",
+        "osmnx",
+        "pandas",
+        "pyproj",
+        "shapely",
+    ],
+    url="https://github.com/Navxihziq/accessgap-utils",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
